920731b0e8ddc059070716c73e45d235eb2b0d6e
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-17 17:20:37 +0200

    aggiunto confirmUser per chidere all'utente la confema per la cancellazione di un fumetto. Bonus completato

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 
 
@@ -6,29 +7,27 @@
 
 @section('main-content')
     <div class="text-center mb-4 text-white ">
-        <h1>lista fumetti</h1>
+        <h1>Lista fumetti</h1>
         <a href="{{route('comics.create')}}" class="btn btn-primary">Aggiungi fumetto</a>
     </div>
     <div class="container">
         <div class="row d-flex justify-content-around">
             @foreach ($comics as $comic)
-                <div class="card mb-5 p-0 " style="width: 18rem;">
-                   <div>
-                        <img src="{{$comic->thumb}}" class="card-img-top h-70" alt="{{$comic->title}}">
+                <div class="card mb-5 p-0" style="width: 18rem;">
                    
-                   </div>
-                        
+                        <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                    
                     <div class="card-body">
                         <h5 class="card-title">{{$comic->title}}</h5>
                         <p class="card-text">Serie:{{$comic->series}}</p>
                         <div class="d-flex ">
-                            <a href="{{route('comics.show',['comic' => $comic->id])}}" class="btn btn-success">Dettagli</a>
+                            <a href="{{route('comics.show',['comic' => $comic->id])}}" class="btn btn-primary">Dettagli</a>
                             <a href="{{route('comics.edit',['comic' => $comic->id])}}" class="btn btn-warning mx-2">Modifica</a>
-                            <form action="{{route('comics.destroy',['comic'=>$comic->id])}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger">Elimina</button>
-                            </form>
+                            
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$comic->id}}">
+                                Elimina
+                              </button>
+                            @include('partials.modal')
                         </div>
                     </div>
                 </div>
